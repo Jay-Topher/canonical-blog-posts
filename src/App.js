2ae6f10570444066fcdb61351b683e0041567c27
Jones Ogolo <jay.topher11@gmail.com>
2022-08-12 10:13:15 +0100

    mount cards on app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import fetchBlogPosts from "./requests/blogPosts";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchBlogPosts(setPosts);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row u-equal-height u-clearfix">
+        {posts.map((post) => (
+          <Card post={post} key={post.id} />
+        ))}
+      </div>
     </div>
   );
 }
